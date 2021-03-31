629b901aade121b616165af8438d5d22912762d1
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-03-31 11:58:43 -0700

    03312021-Update
    Data Binding to Wall view

--- SeawallCalculator/ModelView.cs
+++ SeawallCalculator/ModelView.cs
@@ -9,42 +9,268 @@ using System.Windows;
 namespace SeawallCalculator
 {
     //ModelView collects and displays information from the UI 
-     public class ModelView
+    public class ModelView
     {
-        private ICommand m_ButtonCommand;
+        //Control Properties
+        //ICommand objects that bind the controls in the UI, instead of having them bound directly to their click events
+        //WallControl Properties
+        private ICommand m_WallAnalyzeCommand;
 
-        public ICommand ButtonCommand
+        public ICommand WallAnalyzeButton
         {
             get
             {
-                return m_ButtonCommand;
+                return m_WallAnalyzeCommand;
             }
             set
             {
-                m_ButtonCommand = value;
+                m_WallAnalyzeCommand = value;
             }
         }
-        private ICommand m_SecondCommand;
-        public ICommand SecondCommand
+        private ICommand m_WallReportCommand;
+        public ICommand WallReportButton
         {
             get
             {
-                return m_SecondCommand;
+                return m_WallReportCommand;
             }
             set
             {
-                m_SecondCommand = value;
+                m_WallReportCommand = value;
             }
         }
+        //Wall input Data Properties 
+        private string in_GroundElevation;
+        public string GroundElevation {
+            get
+            {
+                return in_GroundElevation;
+            }
+            set
+            {
+                in_GroundElevation = value;
+            }
+        }
+        private string in_GroundWaterDepth;
+        public string GroundWaterDepth
+        {
+            get
+            {
+                return in_GroundWaterDepth;
+            }
+            set
+            {
+                in_GroundWaterDepth = value;
+            }
+        }
+        private string in_OpenWaterLevel;
+        public string OpenWaterLevel
+        {
+            get
+            {
+                return in_OpenWaterLevel;
+            }
+            set
+            {
+                in_OpenWaterLevel = value;
+            }
+        }
+        private string in_MudlineSlope;
+        public string MudlineSlope
+        {
+            get
+            {
+                return in_MudlineSlope;
+            }
+            set
+            {
+                in_MudlineSlope = value;
+            }
+        }
+        private string in_TopOfPile;
+        public string TopOfPile
+        {
+            get
+            {
+                return in_TopOfPile;
+            }
+            set
+            {
+                in_TopOfPile = value;
+            }
+        }
+        private string in_PilesSpacing;
+        public string PilesSpacing
+        {
+            get
+            {
+                return in_PilesSpacing;
+            }
+            set
+            {
+                in_PilesSpacing = value;
+            }
+        }
+        private string in_LateralCapacityKingPiles;
+        public string LateralCapacityKingPiles
+        {
+            get
+            {
+                return in_LateralCapacityKingPiles;
+            }
+            set
+            {
+                in_LateralCapacityKingPiles = value;
+            }
+        }
+        private string in_SlopeBatteredPiles;
+        public string SlopeBatteredPiles
+        {
+            get
+            {
+                return in_SlopeBatteredPiles;
+            }
+            set
+            {
+                in_SlopeBatteredPiles = value;
+            }
+        }
+        private string in_PanelThickness;
+        public string PanelThickness
+        {
+            get
+            {
+                return in_PanelThickness;
+            }
+            set
+            {
+                in_PanelThickness = value;
+            }
+        }
+        private string in_Penetration;
+        public string Penetration
+        {
+            get
+            {
+                return in_Penetration;
+            }
+            set
+            {
+                in_Penetration = value;
+            }
+        }
+        private string in_SafetyFactor;
+        public string SafetyFactor
+        {
+            get
+            {
+                return in_SafetyFactor;
+            }
+            set
+            {
+                in_SafetyFactor = value;
+            }
+        }
+        public string in_SoilDensity
+        {
+            get
+            {
+                return in_SoilDensity;
+            }
+            set
+            {
+                in_SoilDensity = value;
+            }
+        }
+        private string in_SaturatedSoilDensity;
+        public string SaturatedSoilDensity
+        {
+            get
+            {
+                return in_SaturatedSoilDensity;
+            }
+            set
+            {
+                in_SaturatedSoilDensity = value;
+            }
+        }
+        private string in_ActiveFrictionAngle;
+        public string ActiveFrictionAngle
+        {
+            get
+            {
+                return in_ActiveFrictionAngle;
+            }
+            set
+            {
+                in_ActiveFrictionAngle = value;
+            }
+        }
+        private string in_PassiveFrictionAngle;
+        public string PassiveFrictionAngle
+        {
+            get
+            {
+                return in_PassiveFrictionAngle;
+            }
+            set
+            {
+                in_PassiveFrictionAngle = value;
+            }
+        }
+        private string in_SoilToWallFrictionAngle;
+        public string SoilToWallFrictionAngle
+        {
+            get
+            {
+                return in_SoilToWallFrictionAngle;
+            }
+            set
+            {
+                in_SoilToWallFrictionAngle = value;
+            }
+        }
+        private string in_LiveSurcharge;
+        public string LiveSurcharge
+        {
+            get
+            {
+                return in_LiveSurcharge;
+            }
+            set
+            {
+                in_LiveSurcharge = value;
+            }
+        }
+
+        private CalculationManager calculationManager = new CalculationManager();
+
+        //The model view constructor creates the relay command from the button objects and collects the data from the UI 
         public ModelView()
         {
-            ButtonCommand = new RelayCommand(new Action<object>(ShowMessage));
-            SecondCommand = new RelayCommand(new Action<object>(ShowMessage));
+            //Initializes the controls as ICommand objects 
+            WallAnalyzeButton = new RelayCommand(new Action<object>(AnalyzeWall));
+            WallReportButton = new RelayCommand(new Action<object>(CreateWallReport));
+            
         }
         public void ShowMessage(object obj)
         {
             MessageBox.Show(obj.ToString());
         }
+        private void CheckWallData()
+        {
+            Console.WriteLine("This is the check wall method reporting in");
+        }
+        private void AnalyzeWall(object obj)
+        {
+
+        }
+        private void CreateWallReport(object obj)
+        {
+            throw new  NotImplementedException();
+        }
+
+
 
     }
 }
--- SeawallCalculator/RelayCommand.cs
+++ SeawallCalculator/RelayCommand.cs
@@ -7,8 +7,11 @@ using System.Windows.Input;
 
 namespace SeawallCalculator
 {
+    //RelayCommand implements the ICommand interface and can be used for any control in the views.
+    //From documentation: ICommand provides the commanding behavior for UI elements 
     class RelayCommand:ICommand
     {
+        //Represents a specialized action that can be executed on an item 
         private Action<object> _action;
         public RelayCommand(Action<object> action)
         {
